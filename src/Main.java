28f8749556d960bf28b8eb44b089bb65e923c758
karavauu <aren.poloyan.2@mail.ru>
2023-11-17 20:02:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int quantityscanner = scanner.nextInt();
+
+        for (int i = 0; i < quantityscanner; i++) {
+            System.out.print(20 + " ");
+        }
+    }
+}
